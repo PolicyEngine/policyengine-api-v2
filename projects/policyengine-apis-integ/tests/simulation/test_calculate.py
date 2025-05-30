7a68550b2c2d20e0dff51e4fa35409c124a6c0f7
Nikhil Woodruff <35577657+nikhilwoodruff@users.noreply.github.com>
2025-05-30 12:34:35 +0100

    Correct integration test with `gcs://` data format

--- projects/policyengine-apis-integ/tests/simulation/test_calculate.py
+++ projects/policyengine-apis-integ/tests/simulation/test_calculate.py
@@ -9,7 +9,7 @@ def test_calculation(client: policyengine_simulation_api_client.DefaultApi):
                 "gov.irs.credits.ctc.refundable.fully_refundable": policyengine_simulation_api_client.ParametricReformValue.from_dict({"2023-01-01.2100-12-31":True})
             },
             subsample=200, # reduce the number of households to speed things up.
-            data=policyengine_simulation_api_client.Data("gs://policyengine-us-data/cps_2023.h5") # force the service to use google storage (policyengine.py defaults to huggingface)
+            data=policyengine_simulation_api_client.Data("gcs://policyengine-us-data/cps_2023.h5") # force the service to use google storage (policyengine.py defaults to huggingface)
         )
     response = client.simulate_simulate_economy_comparison_post(
         options
