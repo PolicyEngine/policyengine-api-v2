0e1ed2560ceb63e5021c63aaa99fef89ab3c2564
Anthony Volk <anth.volk@gmail.com>
2025-06-04 16:29:33 -0400

    feat: Add logging to simulation API

--- libs/policyengine-simulation-api/src/policyengine_api/simulation_api/simulation.py
+++ libs/policyengine-simulation-api/src/policyengine_api/simulation_api/simulation.py
@@ -1,12 +1,9 @@
-from typing import Annotated
-import os
-from fastapi import APIRouter, Depends, HTTPException
-from pydantic import BaseModel
+from fastapi import APIRouter
 from policyengine.simulation import SimulationOptions, Simulation
 from policyengine.outputs.macro.comparison.calculate_economy_comparison import (
     EconomyComparison,
 )
-from pathlib import Path
+from policyengine_api.simulation_api.utils.gcp_logging import logger
 
 
 def create_router():
@@ -17,10 +14,26 @@ def create_router():
     )
     async def simulate(parameters: SimulationOptions) -> EconomyComparison:
         model = SimulationOptions.model_validate(parameters)
-        print("Initialising")
+        logger.log_struct(
+            {
+                "message": "Initializing economy comparison simulation",
+                "parameters": parameters.model_dump(),
+            }
+        )
         simulation = Simulation(**model.model_dump())
-        print("Calculating")
+        logger.log_struct(
+            {
+                "message": "Running economy comparison simulation",
+                "parameters": parameters.model_dump(),
+            }
+        )
         result = simulation.calculate_economy_comparison()
+        logger.log_struct(
+            {
+                "message": "Economy comparison simulation completed",
+                "result": result,
+            }
+        )
 
         return result
 
