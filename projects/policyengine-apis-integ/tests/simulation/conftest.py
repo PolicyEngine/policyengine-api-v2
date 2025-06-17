ffff415b020c1d0e180c1ee470aa06164c48562b
SakshiKekre <sakshi.kekre@gmail.com>
2025-06-17 00:17:28 -0700

    revert client retry logic

--- projects/policyengine-apis-integ/tests/simulation/conftest.py
+++ projects/policyengine-apis-integ/tests/simulation/conftest.py
@@ -1,8 +1,6 @@
 import policyengine_simulation_api_client
 import pytest
 from pydantic_settings import BaseSettings, SettingsConfigDict
-from urllib3.util.retry import Retry
-from urllib3 import PoolManager
 
 
 class Settings(BaseSettings):
@@ -19,25 +17,7 @@ settings = Settings()
 @pytest.fixture()
 def client() -> policyengine_simulation_api_client.DefaultApi:
     config = policyengine_simulation_api_client.Configuration(host=settings.base_url)
-
-    # Set up retry logic
-    retries = Retry(
-        total=5,
-        backoff_factor=0.5,
-        status_forcelist=[503],
-        allowed_methods=["HEAD", "GET", "POST", "PUT", "DELETE", "OPTIONS", "TRACE"],
-        raise_on_status=False,
-    )
-
-    # Construct the ApiClient
-    api_client = policyengine_simulation_api_client.ApiClient(configuration=config)
-
-    # Inject the retry logic into the underlying urllib3 pool manager
-    api_client.rest_client.pool_manager = PoolManager(
-        retries=retries
-    )
-
+    client = policyengine_simulation_api_client.ApiClient(config)
     if settings.access_token:
-        api_client.default_headers["Authorization"] = f"Bearer {settings.access_token}"
-
-    return policyengine_simulation_api_client.DefaultApi(api_client)
+        client.default_headers["Authorization"] = f"Bearer {settings.access_token}"
+    return policyengine_simulation_api_client.DefaultApi(client)
