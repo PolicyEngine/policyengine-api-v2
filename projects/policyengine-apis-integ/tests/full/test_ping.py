0c09cb1b1f117e1c7eee09d789373b7e0e804be9
SakshiKekre <sakshi.kekre@gmail.com>
2025-06-17 00:10:04 -0700

    updated tests to retry on 503

--- projects/policyengine-apis-integ/tests/full/test_ping.py
+++ projects/policyengine-apis-integ/tests/full/test_ping.py
@@ -1,11 +1,13 @@
 import policyengine_full_api_client
 import pytest
+from tests.utils.retry_503 import retry_on_503
 
 
 # I don't love this client. We should investigate alternatives.
 # the package structure is really tedious to use
 # it doesn't define methods on the client so it's not very OO
 # and it returns a union instead of just throwing an exception
+@retry_on_503
 def test_ping(client: policyengine_full_api_client.DefaultApi):
     response = client.ping_ping_post(policyengine_full_api_client.PingRequest(value=12))
     assert response.incremented == 13
