34200eddd37903bb2d564062daead2a145d15f99
SakshiKekre <sakshi.kekre@gmail.com>
2025-06-23 16:38:02 -0700

    replaced custom decorator with library decorator: backoff

--- projects/policyengine-apis-integ/tests/full/test_ping.py
+++ projects/policyengine-apis-integ/tests/full/test_ping.py
@@ -1,13 +1,18 @@
 import policyengine_full_api_client
-import pytest
-from tests.utils.retry_503 import retry_on_503
+from policyengine_simulation_api_client.exceptions import ServiceException
+import backoff
 
 
 # I don't love this client. We should investigate alternatives.
 # the package structure is really tedious to use
 # it doesn't define methods on the client so it's not very OO
 # and it returns a union instead of just throwing an exception
-@retry_on_503
+@backoff.on_exception(
+    backoff.expo,
+    ServiceException,
+    max_tries=5,
+    giveup=lambda e: getattr(e, "status", None) != 503,
+)
 def test_ping(client: policyengine_full_api_client.DefaultApi):
     response = client.ping_ping_post(policyengine_full_api_client.PingRequest(value=12))
     assert response.incremented == 13
